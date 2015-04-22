1459beaff3ba0f11535c18198e65a1bad5767c66
Andy Smith <andyrewsmith@github.com>
2015-04-22 03:34:23 +0100

    mobile app

--- helloGeo.js
+++ helloGeo.js
@@ -2,221 +2,266 @@ Tasks = new Mongo.Collection("tasks");
 Profiles = new Mongo.Collection("profiles");
 
 if (Meteor.isClient) {
-  // This code only runs on the client
+    // This code only runs on the client
+    //var recentCheckIns = new Mongo.Collection("recentCheckIns");
+    Meteor.subscribe("recentCheckIns");
 
-  Meteor.startup(function() {
-	//
-  });
+    Meteor.startup(function() {
+        //
+    });
 
-  Template.home.rendered = function(){
-	GoogleMaps.load();
-  };
+    Template.registerHelper("recentCheckIns",function(){
+        time = new Date();
+        var maxtime = new Date(time - 1000 * 60 * 60 * 16);
+        return Tasks.find({
+                createdAt: {
+                    $gt: maxtime
+                }
+            },
+            {
+                sort: {
+                    createdAt: -1
+                }
+            });
+    });
 
-  Template.home.helpers({
-	tasks: function () {
-	  return Tasks.find({}, {sort: {createdAt: -1}});
-	},
-	numberOfCheckins: function () {
-	  return Tasks.find({}).count();
-	},
-	loc: function () {
-	  // return 0, 0 if the location isn't ready
-	  return Geolocation.latLng() || { lat: 0, lng: 0 };
-	},
-	exampleMapOptions: function() {
-	// Make sure the maps API has loaded
-	  if (GoogleMaps.loaded()) {
-		// Map initialization options
-		styles = [];
-		return {
-		  center: new google.maps.LatLng(50.158399799999996,-5.0923081),
-		  zoom: 13,
-		  disableDefaultUI: true,
-		  zoomControl: true,
-		  styles: [
-			{
-			  "stylers": [
-				{ "visibility": "off" }
-			  ]
-			},{
-			  "featureType": "road",
-			  "elementType": "geometry",
-			  "stylers": [
-				{ "color": "#ffffff" },
-				{ "visibility": "simplified" }
-			  ]
-			},{
-			  "featureType": "water",
-			  "elementType": "geometry",
-			  "stylers": [
-				{ "color": "#05a1f4" },
-				{ "visibility": "on" }
-			  ]
-			},{
-			  "featureType": "landscape",
-			  "elementType": "geometry",
-			  "stylers": [
-				{ "visibility": "on" },
-				{ "color": "#000000" }
-			  ]
-			},{
-			  "featureType": "administrative",
-			  "elementType": "labels.text.fill",
-			  "stylers": [
-				{ "visibility": "on" },
-				{ "hue": "#ff2b00" },
-				{ "saturation": 100 },
-				{ "weight": 2.5 },
-				{ "lightness": 100 }
-			  ]
-			}
-		  ]
-		};
-	  }
-	}
-  });
+    Template.home.rendered = function(){
+        GoogleMaps.load();
+    };
 
-  Template.home.events({
-	"submit .new-task": function (event) {
-	  // This function is called when the new task form is submitted
-	  var text = event.target.text.value;
-	  var location = Geolocation.latLng();
-	  var type = event.target.performanceType.value;
-	  var successfulLocation = false;
+    Template.home.helpers({
+        tasks: function () {
+            return UI._globalHelpers.recentCheckIns();
+        },
+        numberOfCheckins: function () {
+            return Tasks.find({}).count();
+        },
+        loc: function () {
+            // return 0, 0 if the location isn't ready
+            return Geolocation.latLng() || { lat: 0, lng: 0 };
+        },
+        exampleMapOptions: function() {
+            // Make sure the maps API has loaded
+            if (GoogleMaps.loaded()) {
+                // Map initialization options
+                return {
+                    center: new google.maps.LatLng(50.158399799999996,-5.0923081),
+                    zoom: 13,
+                    disableDefaultUI: true,
+                    zoomControl: true,
+                    scrollwheel: false,
+                    styles: [
+                        {
+                            "stylers": [
+                                { "visibility": "off" }
+                            ]
+                        },{
+                            "featureType": "road",
+                            "elementType": "geometry",
+                            "stylers": [
+                                { "color": "#ffffff" },
+                                { "visibility": "simplified" }
+                            ]
+                        },{
+                            "featureType": "water",
+                            "elementType": "geometry",
+                            "stylers": [
+                                { "color": "#05a1f4" },
+                                { "visibility": "on" }
+                            ]
+                        },{
+                            "featureType": "landscape",
+                            "elementType": "geometry",
+                            "stylers": [
+                                { "visibility": "on" },
+                                { "color": "#000000" }
+                            ]
+                        },{
+                            "featureType": "administrative",
+                            "elementType": "labels.text.fill",
+                            "stylers": [
+                                { "visibility": "on" },
+                                { "hue": "#ff2b00" },
+                                { "saturation": 100 },
+                                { "weight": 2.5 },
+                                { "lightness": 100 }
+                            ]
+                        }
+                    ]
+                };
+            }
+        }
+    });
 
-	  if(location == null){
-		console.log("failed to get position");
-          return false;
-	  }else{
-		console.log("success?");
-		console.log(location.lat);
-		console.log(location.lng);
-		successfulLocation = true;
-	  }
+    Template.home.events({
+        "submit .new-task": function (event) {
+            // This function is called when the new task form is submitted
+            var text = event.target.text.value;
+            var location = Geolocation.latLng();
+            var type = event.target.performanceType.value;
+            var successfulLocation = false;
 
-	  if(successfulLocation){
-		Tasks.insert({
-		  text: text,
-		  createdAt: new Date(),            // current time
-		  owner: Meteor.userId(),           // _id of logged in user
-		  username: Meteor.user().username,  // username of logged in user
-		  lat: location.lat,  //latitude
-		  lng: location.lng,   //longitude
-		  performanceType: type //performance type
-		});
+            if(location == null){
+                console.log("failed to get position");
+                return false;
+            }else{
+                console.log("success?");
+                console.log(location.lat);
+                console.log(location.lng);
+                successfulLocation = true;
+            }
 
-	  }
+            if(successfulLocation){
+                Meteor.call("checkIn", text, location.lat, location.lng, type, function(error, result){
+                    //do something?
+                });
+            }
 
 
-	  // Clear form
-	  event.target.text.value = "";
+            // Clear form
+            event.target.text.value = "";
 
-	  // Prevent default form submit
-	  return false;
-	}
-  });
+            // Prevent default form submit
+            return false;
+        }
+    });
 
-  Template.task.events({
-	"click .delete": function () {
-	  Tasks.remove(this._id);
-	}
-  });
+    Template.task.events({
+        "click .delete": function () {
+            Meteor.call("deleteCheckIn", this._id);
 
-  Template.task.helpers({
-	date: function(){
-	  return moment(this.createdAt).fromNow();
-	}
-  });
+        }
+    });
 
-  Template.editprofile.helpers({
-	getProfile: function() {
-		return Profiles.findOne({username: Meteor.user().username});
-	}
+    Template.task.helpers({
+        date: function(){
+            return moment(this.createdAt).fromNow();
+        }
+    });
+    Template.editprofile.rendered = function(){
+       //Meteor.subscribe('userProfile', Meteor.user().username);
 
-  });
+        //console.log("subscribing to " + Meteor.user().username);
+    };
+    Template.editprofile.helpers({
+        getProfile: function() {
+            Meteor.subscribe('userProfile', Meteor.user().username);
+            return Profiles.findOne({username: Meteor.user().username});
+        }
 
-  Template.editprofile.events({
-	'submit .editProfile': function(event){
-        var newProfileText = event.target.profiletext.value;
-        Meteor.call("updateProfile", newProfileText);
-        return false;
-    }
-  });
-  Template.home.onCreated(function() {
-	// We can use the `ready` callback to interact with the map API once the map is ready.
-	GoogleMaps.ready('exampleMap', function(map) {
+    });
 
-	  // Add a marker to the map once it's ready
-	  var tasks = Tasks.find().fetch();
+    Template.editprofile.events({
+        'submit .editProfile': function(event){
+            var newProfileText = event.target.profiletext.value;
+            Meteor.call("updateProfile", newProfileText);
+            return false;
+        }
+    });
+    Template.home.onCreated(function() {
+        // We can use the `ready` callback to interact with the map API once the map is ready.
+        GoogleMaps.ready('exampleMap', function(map) {
 
-	  infowindow = new google.maps.InfoWindow({
-				content: "Loading..."
-			});
 
-	  for(i = 0; i < tasks.length; i++){
-		var latlng = new google.maps.LatLng(tasks[i].lat,tasks[i].lng);
+            infowindow = new google.maps.InfoWindow({
+                content: "Loading..."
+            });
 
-		var icon;
+            UI._globalHelpers.recentCheckIns().observeChanges({
+                added: function(id,fields){
 
-		switch(tasks[i].performanceType){
-		  case "music":
-				icon = 'images/map-icon-music.png';
-		  break;
-		  case "theatre":
-				icon = 'images/map-icon-theatre.png';
-		  break;
-		  case "painting":
-				icon = 'images/map-icon-painting.png';
-		  break;
-		  default:
-				icon = 'images/map-icon-default.png';
-		  break;
-		}
+                    addMarker(fields.lat, fields.lng, fields.username, fields.createdAt, fields.text,fields.performanceType);
+                }
+            });
 
-		var marker = new google.maps.Marker({
-		  position: latlng,
-		  icon: icon,
-		  map: map.instance
-		});
-		openInfoWindow(marker, map, tasks[i].username, tasks[i].createdAt, tasks[i].text);
-	  }
+            function addMarker(lat,lng,user,time,text,type){
+                var latlng = new google.maps.LatLng(lat,lng);
+                var icon;
 
-	  function openInfoWindow(marker2, map, name, time, text){
-		google.maps.event.addListener(marker2, 'click', function() {
-		  infowindow.setContent("<strong>"+name+"</strong><br>"+text+"<br>"+moment(time).fromNow());
-		  infowindow.open(map.instance,marker2);
-		  //infowindow.open();
-		});
-	  }
+                switch(type){
+                    case "music":
+                        icon = 'images/map-icon-music.png';
+                        break;
+                    case "theatre":
+                        icon = 'images/map-icon-theatre.png';
+                        break;
+                    case "painting":
+                        icon = 'images/map-icon-painting.png';
+                        break;
+                    default:
+                        icon = 'images/map-icon-default.png';
+                        break;
+                }
 
-	});
-  });
+                var marker = new google.maps.Marker({
+                    position: latlng,
+                    icon: icon,
+                    map: map.instance
+                });
+                addInfoWindow(marker, map, user, time, text);
+            }
+
+            function addInfoWindow(marker2, map, name, time, text){
+                google.maps.event.addListener(marker2, 'click', function() {
+                    infowindow.setContent("<strong>"+name+"</strong><br>"+text+"<br>"+moment(time).fromNow());
+                    infowindow.open(map.instance,marker2);
+                });
+            }
+
+        });
+    });
 
     Template.profile.onCreated(function(){
         var route = Router.current();
-        Meteor.call("getPublicProfile", route.params.username, function(er,res){
+        var user = route.params.username;
+        Meteor.subscribe("allCheckInsForUser", user);
+        Meteor.call("getPublicProfile", user, function(error,res){
             Session.set("profilebio",res);
         });
     });
 
     Template.profile.helpers({
-      usercheckins: function () {
-          return Tasks.find({username: this.username});
-      },
-      userprofile: function () {
-          return Session.get("profilebio") || "Error getting user profile";
-      }
+        usercheckins: function () {
+            return Tasks.find({username: Router.current().params.username}, {sort: {createdAt: -1}});
+        },
+        userprofile: function () {
+            return Session.get("profilebio") || "Error getting user profile";
+        }
     });
 
 
 
     Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
+        passwordSignupFields: "USERNAME_ONLY"
     });
 
 }
 
 if(Meteor.isServer){
+    Meteor.publish("recentCheckIns",function(){
+        time = new Date();
+        var maxtime = new Date(time - 1000 * 60 * 60 * 16);
+        return Tasks.find({
+                createdAt: {
+                    $gt: maxtime
+                }
+            },
+            {
+                sort: {
+                    createdAt: -1
+                }
+            });
+    });
+    Meteor.publish("allCheckIns",function(){
+        return Tasks.find();
+    });
+    Meteor.publish("allCheckInsForUser",function(user){
+        return Tasks.find({username: user});
+    });
+    Meteor.publish("userProfile",function(user){
+        return Profiles.find({username: user});
+    });
+
     Meteor.methods({
         getPublicProfile: function(username){
             return Profiles.findOne({username: username}).profiletext;
@@ -241,24 +286,40 @@ if(Meteor.isServer){
                     profiletext: newProfileText
                 });
             }
-
             return true;
+        },
+        checkIn: function(text,glat,glng,type){
+            if (! Meteor.userId()) {
+                throw new Meteor.Error("not-authorized");
+            }
+            Tasks.insert({
+                text: text,
+                createdAt: new Date(),            // current time
+                owner: Meteor.userId(),           // _id of logged in user
+                username: Meteor.user().username,  // username of logged in user
+                lat: glat,  //latitude
+                lng: glng,   //longitude
+                performanceType: type //performance type
+            });
+        },
+        deleteCheckIn: function(id){
+            Tasks.remove(id);
         }
     });
 }
 
 Router.map(function () {
-  this.route('home', {
-	path: '/',  //overrides the default '/home'
-  });
+    this.route('home', {
+        path: '/',  //overrides the default '/home'
+    });
 
-  this.route('profile', {
-	path: '/profile/:username',
-	data: function () {
-	  return Meteor.users.findOne({username: this.params.username})
-	},
-	template: 'profile'
-  });
+    this.route('profile', {
+        path: '/profile/:username',
+        data: function () {
+            return Meteor.users.findOne({username: this.params.username})
+        },
+        template: 'profile'
+    });
 
-	this.route('editprofile');
+    this.route('editprofile');
 });
---
+++ mobile-config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by andysmith on 22/04/15.
+ */
+
+App.info({
+    id: 'io.fal.urbanow',
+    name: 'urbanow',
+    description: 'Discover local performances',
+    author: 'Andy Smith'
+});
+App.setPreference('fullscreen', true);
+App.accessRule('*.google.com/*');
+App.accessRule('*.googleapis.com/*');
+App.accessRule('*.gstatic.com/*');
